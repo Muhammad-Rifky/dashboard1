9a4785dfc5a63e4c1441a051443ce76f88046bf4
Muhammad Rifky Harto <113489414+Muhammad-Rifky@users.noreply.github.com>
2026-05-17 20:22:28 +0700

    Update route.js

--- src/app/api/devices/update/route.js
+++ src/app/api/devices/update/route.js
@@ -1,49 +1,82 @@
+import db from "@/lib/db";
+
+const lastRequestMap =
+  new Map();
+
 export async function POST(req) {
   try {
     const {
       device_id,
       command = "update",
-      duration = 0
+      duration = 0,
     } = await req.json();
 
-    if (!device_id) {
-      return Response.json(
-        { success: false, message: "device_id wajib" },
-        { status: 400 }
+    const key =
+      `${device_id}-${command}`;
+
+    const now = Date.now();
+
+    const lastTime =
+      lastRequestMap.get(key);
+
+    if (
+      lastTime &&
+      now - lastTime < 2000
+    ) {
+      return Response.json({
+        success: false,
+      });
+    }
+
+    lastRequestMap.set(
+      key,
+      now
+    );
+
+    await fetch(
+      "http://76.13.192.195:3001/publish",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type":
+            "application/json",
+        },
+        body: JSON.stringify({
+          topic: `iot/control/${device_id}`,
+          message:
+            JSON.stringify({
+              command,
+              duration,
+            }),
+        }),
+      }
+    );
+
+    // simpan waktu request update
+    if (command === "update") {
+      await db.query(
+        `
+        UPDATE devices
+        SET last_update_request = NOW()
+        WHERE device_id = ?
+      `,
+        [device_id]
       );
     }
 
-    console.log("📨 SEND TO MQTT:", {
-      device_id,
-      command,
-      duration
-    });
-
-    const res = await fetch("http://76.13.192.195:3001/publish", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        topic: `iot/control/${device_id}`,
-        message: JSON.stringify({
-          command,
-          duration
-        }),
-      }),
-    });
-
-    const data = await res.json();
-
     return Response.json({
       success: true,
-      message: `Command ${command} sent to ${device_id}`,
-      data,
     });
 
   } catch (err) {
-    console.error("API ERROR:", err);
     return Response.json(
-      { success: false, error: err.message },
-      { status: 500 }
+      {
+        error:
+          err.message,
+      },
+      {
+        status: 500,
+      }
     );
   }
 }
