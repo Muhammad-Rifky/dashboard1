59351299e583b5ece3c0d0b7471754ce1a09ae08
Muhammad Rifky Harto <113489414+Muhammad-Rifky@users.noreply.github.com>
2026-06-10 23:02:03 +0700

    Add timezone and dateStrings to DB config

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -5,12 +5,14 @@ const config = {
   user: "iotuser",
   password: "123456",
   database: "iot_system",
+  timezone: "+07:00",
+  dateStrings: "true",
 };
 
 const db = mysql.createPool(config);
 
 // debug aman
-console.log("🔥 DB CONNECTED:", {
+console.log("DB CONNECTED:", {
   host: config.host,
   database: config.database,
 });
