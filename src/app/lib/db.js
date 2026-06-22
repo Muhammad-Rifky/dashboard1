074bc693a6c64243e8ed66bddcde57469c957794
Muhammad Rifky Harto <113489414+Muhammad-Rifky@users.noreply.github.com>
2026-06-22 18:37:17 +0700

    Change DB password environment variable to DB_PASS

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   timezone: "+07:00",
   dateStrings: true,
