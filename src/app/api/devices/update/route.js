f62a56ca888111a8bf79953cbf5ad18e012e3624
Muhammad Rifky Harto <113489414+Muhammad-Rifky@users.noreply.github.com>
2026-06-29 12:17:10 +0700

    Update MQTT fetch URL to include '/publish'

--- src/app/api/devices/update/route.js
+++ src/app/api/devices/update/route.js
@@ -33,16 +33,16 @@ console.log("Message:", message);
 let mqttRes;
 
 try {
-  mqttRes = await fetch("iot-aqua-rifky.duckdns.org", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      topic,
-      message,
-    }),
-  });
+  mqttRes = await fetch("https://iot-aqua-rifky.duckdns.org/publish", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    topic,
+    message,
+  }),
+});
 
   console.log("MQTT STATUS:", mqttRes.status);
   console.log("MQTT STATUS TEXT:", mqttRes.statusText);
