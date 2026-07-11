18ae37e1872f537ffa2340da5741f180394163d8
Muhammad Rifky Harto <113489414+Muhammad-Rifky@users.noreply.github.com>
2026-07-11 20:12:13 +0700

    Update database configuration to use environment variables

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,19 +1,20 @@
 import mysql from "mysql2/promise";
 
 const config = {
-  host: "localhost",
-  user: "iotuser",
-  password: "123456",
-  database: "iot_system",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   timezone: "+07:00",
   dateStrings: true,
 };
 
 const db = mysql.createPool(config);
 
-// debug aman
 console.log("DB CONNECTED:", {
   host: config.host,
+  port: config.port,
   database: config.database,
 });
 
